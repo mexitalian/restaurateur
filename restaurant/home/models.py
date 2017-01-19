046a6cb185d9596ad67920f180b1c50f1f9c0abb
Giovanni Carlo Marasco <mexitalian@gmail.com>
2017-01-18 23:09:27 -0600

    Creates models needed to build the Menu page from the Admin UI. Menu items -> sections (snippets) -> placement (orderable) -> page Creates quick and dirty template to check values are coming through to the menu_page template.

--- restaurant/home/models.py
+++ restaurant/home/models.py
@@ -2,8 +2,73 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from wagtail.wagtailcore.models import Orderable, Page
+from wagtail.wagtailsnippets.models import register_snippet
+from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, FieldRowPanel, MultiFieldPanel #, StreamFieldPanel
 
+# -------------------
+#   HomePage Models
+# -------------------
 
 class HomePage(Page):
     pass
+
+
+# ---------------
+#   Menu Models
+# ---------------
+
+class MenuItem(models.Model):
+    section = ParentalKey('MenuSection', related_name='menu_items')
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    recommend = models.BooleanField()
+
+    panels = [
+        FieldPanel('name'),
+        FieldRowPanel(children=[
+            FieldPanel('price', classname='col8'),
+            FieldPanel('recommend', classname='col4')
+        ]),
+        FieldPanel('description'),
+    ]
+
+
+@register_snippet
+class MenuSection(ClusterableModel):
+    name = models.CharField(max_length=100)
+
+    panels = [
+        FieldPanel('name'),
+        MultiFieldPanel([
+            InlinePanel('menu_items', min_num=1)
+        ], heading='Add items')
+    ]
+
+    def __str__(self):
+        return self.name
+
+class MenuPageSectionPlacement(Orderable, models.Model):
+    page = ParentalKey('MenuPage', related_name='section_placements')
+    section = models.ForeignKey('MenuSection', related_name='+')
+
+    class Meta:
+        verbose_name = "section placement"
+        verbose_name_plural = "section placements"
+
+    panels = [
+        SnippetChooserPanel('section'),
+    ]
+
+    def __str__(self):
+        return self.page.title + " -> " + self.section.name
+
+
+class MenuPage(Page):
+    content_panels = Page.content_panels + [
+        InlinePanel('section_placements', label="Sections"),
+    ]
